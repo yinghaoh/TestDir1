96d2a6da017bc1840df0040cd45060dc5c36a45a
yinghaoh <yinghaoh85@gmail.com>
2015-04-09 16:06:15 -0400

    Updates

--- citiBikeFun.R
+++ citiBikeFun.R
@@ -1,4 +1,6 @@
-myfunction <- function(month_start,year_start,month_end,year_end) {
+library("car")
+
+citiBikeFunc <- function(month_start,year_start,month_end,year_end) {
   
   if (year_start>year_end | month_start<0 | month_start>12 | month_end<0 | month_end>12 | (year_start==year_end & month_start>month_end)){
     print("Error in signature! Please reinput start and end month and year.")
@@ -8,18 +10,19 @@ myfunction <- function(month_start,year_start,month_end,year_end) {
     monthStr <- toString(month)
     if(month<10)monthStr = paste("0",monthStr,sep="")
     year<-year_start
-    while(year!=year_end & month != month_end){        
+    while( (month<=month_end & year==year_end) || year<year_end ){ 
+      print(year)
       x <- read.csv(file=paste("C:/Users/yhuang/Desktop/CitiBike data/citibike-tripdata/citibike-tripdata/",toString(year),"-",monthStr," - Citi Bike trip data.csv",sep=""), head=TRUE,sep=",")
       #Remove null cases
       x = x[complete.cases(x),]
       x = x[x$birth.year!="\\N",]
+      x = x[x$gender!=0,]
       x$birth.year = 97-as.numeric(x$birth.year)
       #Create dummy for user type
       x$userTypeNum[x$usertype=="Customer"] <- "1"
       x$userTypeNum[x$usertype=="Subscriber"] <- "2"
       id<-grep("^userTypeNum$", colnames(x))
       x[,id] <- as.numeric(x[,id])
-      #Convert start time to hour
       id = grep("^starttime$", colnames(x))
       x$triphour<-as.numeric(substr(x$starttime, 12, 13))
       x$weekDay <- weekdays(as.Date(x$starttime))
@@ -32,7 +35,7 @@ myfunction <- function(month_start,year_start,month_end,year_end) {
       x$weekDay[x$weekDay=="Sunday"] <- "2" 
       id<-grep("^weekDay$", colnames(x))
       x[,id] <- as.numeric(x[,id])
-      x$trail <- as.numeric(with(x,paste(start.station.id,end.station.id,sep="")))
+      x$trail <- as.numeric(with(x,paste(x$start.station.id,x$end.station.id,sep="")))
       
       colName_Lean <-c("tripduration","triphour","start.station.id","end.station.id","userTypeNum","birth.year","gender","weekDay","trail")
       id <- colnames(x) %in% colName_Lean
@@ -45,15 +48,80 @@ myfunction <- function(month_start,year_start,month_end,year_end) {
       groupResultsListByHour <- printMeanAndSdByGroup(x_lean[c(1,4,5,6,8)],x_lean[7])       
       groupResultsListByGender <- printMeanAndSdByGroup(x_lean[c(1,4,6,7,8)],x_lean[5])  
       groupResultsListByWeekday <- printMeanAndSdByGroup(x_lean[c(1,4,5,6,7)],x_lean[8]) 
-      #Histogram of station - By weekday & gender
+      groupResultsListByHourFinal <-list()
+      groupResultsListByGenderFinal <- list()
+      groupResultsListByWeekdayFinal <- list()
+      if(year==year_start & month == month_start){
+        groupResultsListByHourFinal[[1]] <- cbind(groupResultsListByHour[[1]],groupResultsListByHour[[2]])
+        groupResultsListByGenderFinal[[1]] <- cbind(groupResultsListByGender[[1]],groupResultsListByGender[[2]])
+        groupResultsListByWeekdayFinal[[1]] <- cbind(groupResultsListByWeekday[[1]],groupResultsListByWeekday[[2]])
+      }else{
+        groupResultsListByHourFinal[[length(groupResultsListByHourFinal)+1]] <- cbind(groupResultsListByHour[[1]],groupResultsListByHour[[2]])
+        groupResultsListByGenderFinal[[length(groupResultsListByGenderFinal)+1]] <- cbind(groupResultsListByGender[[1]],groupResultsListByGender[[2]])
+        groupResultsListByWeekdayFinal[[length(groupResultsListByWeekdayFinal)+1]] <- cbind(groupResultsListByWeekday[[1]],groupResultsListByWeekday[[2]])
+      }
+
+      #Histogram of start and station and trail - By weekday & gender & hour
+      trailList <- sort(table(x_lean[9]),decreasing = T)[1:10]
+      startList <- sort(table(x_lean[2]),decreasing = T)[1:10]
+      endList <- sort(table(x_lean[3]),decreasing = T)[1:10]
+      nL <- as.numeric(names(trailList))
+      trailList <- rbind(trailList,nL)
+      rownames(trailList) <- NULL
+      nL <- as.numeric(names(startList))
+      startList <- rbind(startList,nL)
+      rownames(startList) <- NULL
+      nL <- as.numeric(names(endList))
+      endList <- rbind(endList,nL)
+      rownames(endList) <- NULL
       
+      if(year==year_start & month == month_start){
+        trailListFinal <- trailList
+        startListFinal <- startList
+        endListFinal <- endList
+      }else{
+        trailListFinal <- cbind(trailListFinal,trailList)
+        startListFinal <- cbind(startListFinal,startList)
+        endListFinal <- cbind(endListFinal,endList)
+      }
       
+      #Correlation analysis      
       if(month == 12){
         year = year + 1
         month = 1
       } 
       else month = month + 1     
     }
+    
+    groupResultsListByHourFinal <- Reduce("+", groupResultsListByHourFinal) / length(groupResultsListByHourFinal)
+    groupResultsListByGenderFinal <- Reduce("+", groupResultsListByGenderFinal) / length(groupResultsListByGenderFinal)
+    groupResultsListByWeekdayFinal <- Reduce("+", groupResultsListByWeekdayFinal) / length(groupResultsListByWeekdayFinal)
+    
+    trailListFinalHist <- sapply(split(1:ncol(trailListFinal),trailListFinal[2,]), function(i) sum(trailListFinal[1,i]))
+    startListFinalHist <- sapply(split(1:ncol(startListFinal),startListFinal[2,]), function(i) sum(startListFinal[1,i]))
+    endListFinalHist <- sapply(split(1:ncol(endListFinal),endListFinal[2,]), function(i) sum(endListFinal[1,i]))
+  
+    trailListFinalHist <- sort(trailListFinalHist, decreasing=TRUE)[1:10]
+    startListFinalHist <- sort(startListFinalHist, decreasing=TRUE)[1:10]
+    endListFinalHist <- sort(endListFinalHist, decreasing=TRUE)[1:10]
+    
+    write.table(groupResultsListByHourFinal, file="groupResultsList.csv", sep=",", col.names=NA)
+    write.table(groupResultsListByGenderFinal, file="groupResultsList.csv",sep=",", append = TRUE,col.names=NA)
+    write.table(groupResultsListByWeekdayFinal, file="groupResultsList.csv", sep=",",append = TRUE,col.names=NA)
+        
+    trailListFinalHist <- data.frame(trailListFinalHist)
+    startListFinalHist <- data.frame(startListFinalHist)
+    endListFinalHist <- data.frame(endListFinalHist)
+    
+    names(trailListFinalHist) <- "Trail Count Average"
+    names(startListFinalHist) <- "Start Station Count Average"
+    names(endListFinalHist) <- "End Station Count Average"
+     
+    write.table(trailListFinalHist, file="StationListFinalHist.csv", sep=",", col.names=NA)
+    write.table(startListFinalHist, file="StationListFinalHist.csv",sep=",", append = TRUE,col.names=NA)
+    write.table(endListFinalHist, file="StationListFinalHist.csv",sep=",", append = TRUE,col.names=NA)
+    
+    print("Success!")
   } 
   
 }
@@ -66,22 +134,35 @@ printMeanAndSdByGroup <- function(variables,groupvariable)
   groupvariable <- groupvariable[,1] # ensures groupvariable is not a list
   means <- aggregate(as.matrix(variables) ~ groupvariable, FUN = mean)
   names(means) <- variablenames
-  print(paste("Means:"))
-  print(means)
+
   # within each group, find the standard deviation of each variable:
   sds <- aggregate(as.matrix(variables) ~ groupvariable, FUN = sd)
   names(sds) <- variablenames
-  print(paste("Standard deviations:"))
-  print(sds)
+
   # within each group, find the number of samples:
   samplesizes <- aggregate(as.matrix(variables) ~ groupvariable, FUN = length)
   names(samplesizes) <- variablenames
-  print(paste("Sample sizes:"))
-  print(samplesizes)
   
   return(list(meanMatrix=means,sdMatrix=sds))
 }
 
 statSummaryMonthly <- function(x_lean){
   
+}
+
+
+mosthighlycorrelated <- function(mydataframe,numtoreport)
+{
+  # find the correlations
+  cormatrix <- cor(mydataframe)
+  # set the correlations on the diagonal or lower triangle to zero,
+  # so they will not be reported as the highest ones:
+  diag(cormatrix) <- 0
+  cormatrix[lower.tri(cormatrix)] <- 0
+  # flatten the matrix into a dataframe for easy sorting
+  fm <- as.data.frame(as.table(cormatrix))
+  # assign human-friendly names
+  names(fm) <- c("First.Variable", "Second.Variable","Correlation")
+  # sort and print the top n correlations
+  head(fm[order(abs(fm$Correlation),decreasing=T),],n=numtoreport)
 }
--- clusterFun.R
+++ clusterFun.R
@@ -1,36 +1,90 @@
 library(tm)
+library(twitteR)
 library(proxy)
+library(wordcloud)
+library(fpc)
+library(topicmodels)
 
-clusteringFun <- function(numCluster){
+clusteringFun <- function(numCluster,sparsity,factor,flag,flag_topic,numTopic){
   # change this file location to suit your machine
-  file_loc <- "C:\\Documents and Settings\\Administrator\\Desktop\\Book1.csv"
+  file_loc <- "trdata.csv"
   # change TRUE to FALSE if you have no column headings in the CSV
-  x <- read.csv(file_loc, header = TRUE)
+  trdata <- read.csv(file_loc, header = FALSE, sep="\n",strip.white=TRUE)
+  trdata$id <- matrix(1:length(trdata[,1]),length(trdata[,1]),1) 
+  colnames(trdata)[1]<-"Text"
   require(tm)
-  dataset <- Corpus(DataframeSource(x))  
-  dataset <- tm_map(dataset, as.PlainTextDocument)
+  dataset <- Corpus(DataframeSource(as.data.frame(trdata$Text)))  
   dataset <- tm_map(dataset, tolower)
-  dataset <- tm_map(dataset, removeWords, stopwords("english"))
-  dataset <- tm_map(dataset, removePunctuation)
+  dataset <- tm_map(dataset, removeNumbers) 
+  dataset <- tm_map(dataset, removeWords, stopwords("english"))  
+  dataset <- tm_map(dataset, removePunctuation)  
+  #stem document
+  #datasetCopy <- dataset
+  #dataset <- tm_map(dataset, stemDocument)
+  #dataset <-tm_map(dataset, stemCompletion, dictionary = datasetCopy)
+  #######################
   dataset <- tm_map(dataset, stripWhitespace)
-  dtm <- DocumentTermMatrix(dataset)
+
+  for(i in 1:10){
+    writeLines(dataset[[i]])
+  }
+  dataset <- tm_map(dataset,PlainTextDocument)
   
-  findFreqTerms(dtm, 100)
-  #Kmeans
-  dtm_tfxidf <- weightTfIdf(dtm)
-  m <- as.matrix(dtm_tfxidf)
-  rownames(m) <- 1:nrow(m)
+  dtm <- DocumentTermMatrix(dataset,control=list(wordLengths=c(2,Inf), bounds = list(global = c(2,floor(length(trdata[,1])*factor))),
+                                                weighting = function(x)
+                                                  weightTfIdf(x, normalize = TRUE)))
+  tdmTF <- TermDocumentMatrix(dataset,control=list(wordLengths=c(2,Inf),bounds = list(global = c(2,floor(length(trdata[,1])*factor))),
+                                                 weighting = function(x)
+                                                   weightTf(x)))
+  #Remove empty entry
+  rowTotals <- apply(dtm , 1, sum) 
+  trdata <- trdata[which(rowTotals!=0),]
+  dtm <- dtm[rowTotals> 0,]  
+  colTotals <- apply(tdmTF , 2, sum) 
+  tdmTF <- tdmTF[ ,colTotals> 0]
+
+  #Remove duplicates
+  trdata <- trdata[!duplicated(dtm,MARGIN=1),]
+  dataset <- dataset[!duplicated(dtm,MARGIN=1)]
+  dtm <- unique(dtm,MARGIN=1)
+  tdmTF <- unique(tdmTF,MARGIN=2)
+   
+  findFreqTerms(tdmTF, 100)
+  #WordCloud
+  m <- as.matrix(tdmTF)
+  word.freq <- sort(rowSums(m), decreasing =T)
+  wordcloud(words = names(word.freq), freq = word.freq, min.freq = 15, random.order = F)
   
+  if(flag)
+    dtm <- removeSparseTerms(dtm, sparse = sparsity)
+  set.seed(100)
+  m2 <- as.matrix(dtm)
+  rownames(m2) <- 1:nrow(m2)   
   norm_eucl <- function(m) m/apply(m, MARGIN=1, FUN=function(x) sum(x^2)^.5)
-  m_norm <- norm_eucl(m)
-  cl <- kmeans(m_norm, numCluster)
+  m_norm <- norm_eucl(m2)
+  cl <- kmeans(m_norm, numCluster,nstart=25)
+  trdata$cluster <- matrix(cl$cluster,length(cl$cluster),1) 
   
-  table(cl$cluster)
+  for(i in 1:numCluster){
+    cat(paste("cluster",i,": ",sep = ""))
+    s<-sort(cl$centers[i,], decreasing = T)
+    cat(names(s)[1:5],"\n")
+  }
   
-  plot(prcomp(m_norm)$x, col=cl$cl)
-  
-  findFreqTerms(dtm[cl$cluster==1], 50)
-  
+  print(table(cl$cluster))
+  write.csv(trdata, file=paste("Result_NumCluster#",numCluster,".csv",sep=""))
+  plot(prcomp(m_norm)$x,col=(cl$cluster)+1, main = "K-Means Clustering Results", xlab = "", ylab = "", pch = 20, cex = 2)
+
+  if(flag_topic){
+    lda = LDA(as.DocumentTermMatrix(tdmTF),k=numTopic)
+    term <- apply(terms(lda,5), MARGIN = 2, paste, collapse = ", ")
+    term
+    #for(i in 1:numTopic){
+    print(topics(lda,1))
+    #}
+    
+  }
+
   #Hierarchical clustering
   #d <- dist(m, method="cosine")
   #hc <- hclust(d, method="average")
